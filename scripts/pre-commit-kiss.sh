1699ebfe571586e9275ca0e75002f2e565e5483b
David Sweet <dsweet@andamooka.org>
2026-04-14 18:37:38 -0400

    ok

--- scripts/pre-commit-kiss.sh
+++ scripts/pre-commit-kiss.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Use the repo-local `kiss` binary (release build; not a globally installed one).
-cargo run --quiet --release -- check --ignore fake_
-
+# cargo run --quiet --release -- check --ignore fake_
+kiss check
